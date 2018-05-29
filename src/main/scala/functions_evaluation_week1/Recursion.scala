0e89bb5def2d3436338728a83c0a0bc4245d1dc9
Mitra <bhanumchandra@gmail.com>
2018-05-28 19:53:40 -0500

    implement a recursive function to verify if an expression has balanced parentheses.

--- src/main/scala/functions_evaluation_week1/Recursion.scala
+++ src/main/scala/functions_evaluation_week1/Recursion.scala
@@ -3,10 +3,10 @@ package functions_evaluation_week1
 object Recursion extends App {
 
   /**
-    *     1
-    *    1 1
-    *   1 2 1
-    *  1 3 3 1
+    * 1
+    * 1 1
+    * 1 2 1
+    * 1 3 3 1
     * 1 4 6 4 1
     * ...
     * Computes the elements of Pascal’s triangle by means of a recursive process.
@@ -16,6 +16,24 @@ object Recursion extends App {
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
+
+  /**
+    * Verifies if a string has balanced parentheses
+    */
+  def balance(chars: List[Char]): Boolean = {
+
+    def countOpen(c: Char): Int = if (c.equals('(')) 1 else 0
+
+    def countClose(c: Char): Int = if (c.equals(')')) -1 else 0
+
+    def loop(chars: List[Char], open: Int, close: Int): Boolean = {
+      if (chars.isEmpty && (open + close) == 0) true
+      else if (chars.isEmpty || (open + close) < 0) false
+      else loop(chars.tail, open + countOpen(chars.head), close + countClose(chars.head))
+    }
+
+    loop(chars, 0, 0)
+  }
 
 
 }
