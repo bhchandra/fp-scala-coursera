c37201298902f849b908381493ff199c9b6a140b
Mitra <bhanumchandra@gmail.com>
2018-06-10 12:40:01 -0500

    simple impl of `last` and `init` on Lists.

---
+++ src/main/scala/week5_Lists/ListsLab.scala
@@ -0,0 +1,25 @@
+package week5_Lists
+
+object ListsLab {
+
+
+  /**
+    * gives the last element of the list
+    */
+  def last[T](xs: List[T]): T = xs match {
+    case List() => throw new Error("last of empty list")
+    case List(x) => x
+    case y :: ys => last(ys)
+  }
+
+
+  /**
+    * all the elements the the list except the last one.
+    */
+  def init[T](xs: List[T]): List[T] = xs match {
+    case List() => throw new Error("init of empty list")
+    case List(x) => List()
+    case y :: ys => y :: init(ys)
+  }
+
+}
